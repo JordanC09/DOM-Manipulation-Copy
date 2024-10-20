fc6117e982ade819778fb88da4888a61588ded6b
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2024-10-20 17:12:09 -0400

    jim croce

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
 const DOMSelectors = {
     
     
-    form: document.querySelector(".form"),
-    
-    container: document.querySelector(".container"),
-    card: document.querySelector(".card"),
-    button: document.querySelector(".remove"),
+  form: document.querySelector(".form"),
   
-    NameInput: document.getElementById("name-input"),
-    DescInput: document.getElementById("Desc-input"),
-    ImageInput: document.getElementById("Image-input"),
-    ColorInput: document.getElementById("Color-input"),
+  container: document.querySelector(".container"),
+  card: document.querySelector(".card"),
+  button: document.querySelector("#getrid"),
+  
+  
+
+  NameInput: document.getElementById("name-input"),
+  DescInput: document.getElementById("Desc-input"),
+  ImageInput: document.getElementById("Image-input"),
+  ColorInput: document.getElementById("Color-input"),
   };
-  console.log(DOMSelectors.text);
-  let id = 0;
-  
-  function makecard() {
-    let name = DOMSelectors.NameInput.value;
-    let desc = DOMSelectors.DescInput.value;
-    let image = DOMSelectors.ImageInput.value;
-    let color = DOMSelectors.ColorInput.value;
-  
-    DOMSelectors.container.insertAdjacentHTML(
-      "beforeend",
-  
-      `
-      <div class = "card" id = "${id}">
-        <h2 class = "card-header"> ${name} </h2>
-        <img class = card-image src="${image}" alt="image made" class = "card-image">
-        <p>${desc}</p>
-        <button class="remove">Remove</button>
-      </div>
-      
-      `
-    );
-    let cardcolor = document.getElementById(id);
-    cardcolor.style.backgroundColor = color;
-  }
+console.log(DOMSelectors.text);
+let id = 0;
 
-  function clearfields(){
-    DOMSelectors.NameInput.value = "";
-    DOMSelectors.DescInput.value = "";
-    DOMSelectors.ImageInput.value = "";
-    DOMSelectors.ColorInput.value = "";
-  }
-  
-  
-  
-  /*DOMSelectors.button.addEventListener("click", function (event) {
-    console.log(event.target.parentElement);
-    event.target.parentElement.style.backgroundColor = "red";
-  });*/
-  DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log(document.querySelector("input").value);
-    makecard();
-    clearfields();
-  });
+function makecard() {
+  let name = DOMSelectors.NameInput.value;
+  let desc = DOMSelectors.DescInput.value;
+  let image = DOMSelectors.ImageInput.value;
+  let color = DOMSelectors.ColorInput.value;
 
-  DOMSelectors.button.addEventListener("click", function (event){
-    event.preventDefault();
-    document.querySelector(".card").remove();
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+
+    `
+    <div class = "card" id = "${id}">
+      <h2 class = "card-header"> ${name} </h2>
+      <img class = card-image src="${image}" alt="image made" class = "card-image">
+      <p>${desc}</p>
+      <button id ="getrid">Remove</button>
+    </div>
     
-    
-  })
+    `
+  );
+  let cardcolor = document.getElementById(id);
+  cardcolor.style.backgroundColor = color;
+}
 
-  /*function backgroundandtext(background, text) {
-    background.style.backgroundColor = "blue";
-    text.style.fontSize = "50px";
-  }
+function clearfields(){
+  DOMSelectors.NameInput.value = "";
+  DOMSelectors.DescInput.value = "";
+  DOMSelectors.ImageInput.value = "";
+  DOMSelectors.ColorInput.value = "";
+}
+
+function eliminate(event){
   
-  backgroundandtext(DOMSelectors.body, DOMSelectors.text);*/
+  event.preventDefault();
+  document.querySelector(".card").remove();
+}
+
+
+/*DOMSelectors.button.addEventListener("click", function (event) {
+  console.log(event.target.parentElement);
+  event.target.parentElement.style.backgroundColor = "red";
+});*/
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //console.log(document.querySelector("input").value);
+  makecard();
+  clearfields();
+});
+
+DOMSelectors.button.addEventListener("click", function (event){
+  event.preventDefault();
+  eliminate();
+})
